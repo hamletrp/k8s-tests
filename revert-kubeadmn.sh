7f0c8f1c687caa45e0cf31a678061144b85a8b8b
Hamlet A. Rodriguez <hamletrp@gmail.com>
2025-04-30 15:19:26 -0400

    update remote repo

--- revert-kubeadmn.sh
+++
@@ -1,9 +0,0 @@
-sudo kubeadm reset --force
-
-sudo iptables -F && sudo iptables -t nat -F && sudo iptables -t mangle -F && sudo iptables -X
-
-sudo rm -rf /etc/cni/net.d && sudo rm -rf /var/lib/etcd && sudo rm -rf /var/lib/kubelet && sudo rm -rf /etc/kubernetes && sudo rm -rf ~/.kube
-
-sudo rm -rf /etc/cni/net.d/*
-
-# sudo reboot
--- test.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-while true; do
-    echo "Hello";
-    sleep 3;
-done
