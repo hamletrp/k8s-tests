15a25ceed6dd816325a5c865fb4d7f85c9ed164f
Hamlet A. Rodriguez <hamletrp@gmail.com>
2025-06-02 15:47:57 -0400

    karpenter deploy test

--- check-pod-status.sh
+++ check-pod-status.sh
@@ -1,8 +1,12 @@
 echo "👉 Checking VPC CNI Add-on..." && \
-eksctl get addon --name vpc-cni --cluster <your-cluster-name> --region <your-region> && \
+eksctl get addon --name vpc-cni --cluster cluster-lab-3 --region us-east-1 && \
 echo -e "\n👉 aws-node DaemonSet status:" && \
 kubectl get daemonset aws-node -n kube-system -o wide && \
 echo -e "\n👉 VPC CNI Image Version(s):" && \
 kubectl get daemonset aws-node -n kube-system -o jsonpath='{.spec.template.spec.containers[*].image}' && echo && \
 echo -e "\n👉 VPC CNI Pod Health:" && \
 kubectl get pods -n kube-system -l k8s-app=aws-node -o wide
+
+# eksctl get addon --name vpc-cni --cluster cluster-lab-3
+
+# k logs -n kube-system -l app.kubernetes.io/instance: aws-vpc-cni
