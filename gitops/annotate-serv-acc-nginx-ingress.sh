15deaff0d102aab43e5080a9d0397f94c70ba2ac
Hamlet A. Rodriguez <hamletrp@gmail.com>
2025-06-06 19:59:14 -0400

    update remote repo

--- gitops/annotate-serv-acc-nginx-ingress.sh
+++ gitops/annotate-serv-acc-nginx-ingress.sh
@@ -6,8 +6,8 @@
 
 # === Update these variables ===
 # ROLE_ARN="arn:aws:iam::722249351142:role/eks-alb-controller-role"
-# ROLE_ARN="arn:aws:iam::722249351142:role/eks-alb-controller-role-cluster-lab-2"
-ROLE_ARN="arn:aws:iam::722249351142:role/eks-alb-controller-role-cluster-lab-2"
+# ROLE_ARN="arn:aws:iam::722249351142:role/eks-alb-controller-role-cluster-lab-1"
+ROLE_ARN="arn:aws:iam::722249351142:role/eks-alb-controller-role-cluster-lab-1"
 NAMESPACE="ingress-nginx"
 SA_NAME="ingress-nginx"
 
--- gitops/annotate-serv-acc-vpc-cni.sh
+++ gitops/annotate-serv-acc-vpc-cni.sh
@@ -5,7 +5,7 @@
 # =============================
 
 # === Update these variables ===
-ROLE_ARN="arn:aws:iam::722249351142:role/eksctl-cluster-lab-2-nodegroup-linu-NodeInstanceRole"
+ROLE_ARN="arn:aws:iam::722249351142:role/eksctl-cluster-lab-1-nodegroup-linu-NodeInstanceRole"
 NAMESPACE="kube-system"
 SA_NAME="aws-node"
 
